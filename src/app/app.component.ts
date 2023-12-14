cc59f75734a54a2603409afb4754debdea864fcd
Armen Vardanyan <armenvardanyan95@gmail.com>
2023-12-14 17:41:10 +0400

    Solution

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,7 @@ import { KnobModule } from 'primeng/knob';
 import { MultiSelectModule } from 'primeng/multiselect';
 import { PanelModule } from 'primeng/panel';
 import { TabViewModule } from 'primeng/tabview';
-import { Subject, forkJoin, interval } from 'rxjs';
+import { Subject, forkJoin, interval, timer } from 'rxjs';
 import { map, scan, startWith, switchMap, takeWhile } from 'rxjs/operators';
 import { TimeInfo } from './time-info.type';
 import { TimeService } from './time.service';
@@ -31,11 +31,43 @@ import { TimeService } from './time.service';
 })
 export class AppComponent {
   // CLOCK
+  currentTime = toSignal(interval(950).pipe(map(() => new Date())), {
+    initialValue: new Date(),
+  });
 
   // TIMER
   selectedMinutes = signal<number | null>(null);
   selectedSeconds = signal<number | null>(null);
   startTimer$ = new Subject<void>();
+  timeLeft = toSignal(
+    this.startTimer$.pipe(
+      switchMap(() => {
+        const overallTime =
+          (this.selectedMinutes() ?? 0) * 60 + (this.selectedSeconds() ?? 0);
+        return timer(0, 1_000).pipe(
+          // startWith(0),
+          scan((acc) => {
+            return this.paused() ? acc : acc + 1;
+          }, 0),
+          map((n) => {
+            return overallTime - n + 1;
+          }),
+          takeWhile((n) => n > 0, true)
+        );
+      })
+    ),
+    { initialValue: null }
+  );
+  timeLeftDisplay = computed(() => {
+    const timeLeft = this.timeLeft();
+
+    if (timeLeft === null || timeLeft === 0) {
+      return null;
+    }
+    const minutes = Math.floor(timeLeft / 60);
+    const seconds = timeLeft % 60;
+    return { minutes, seconds };
+  });
   paused = signal(false);
   // WORLD TIME
   private readonly timeService = inject(TimeService);
@@ -47,6 +79,22 @@ export class AppComponent {
     { timezone: 'Asia/Yerevan' } as TimeInfo,
     { timezone: 'Europe/Kyiv' } as TimeInfo,
   ]);
+  selectedTimezones = computed(() =>
+    this.locations().map((location) => location.timezone)
+  );
+
+  timeInLocations = toSignal(
+    toObservable(this.locations).pipe(
+      switchMap((locations) => {
+        return forkJoin(
+          locations.map((location) => {
+            return this.timeService.getTimeByTimezone(location.timezone);
+          })
+        );
+      })
+    ),
+    { initialValue: [] },
+  );
 
   addLocation(timezones: string[]) {
     this.locations.set(timezones.map((timezone) => ({ timezone } as TimeInfo)));
